f5fa96de12105cdf0d3a46a7cab2b3f74e1474b6
GiancarloC <gcalle288@gmail.com>
2021-03-28 17:03:20 -0400

    finished followme project

---
+++ FollowMe/app/src/main/java/com/example/followme/db/viewmodel/ScoreViewModel.java
@@ -0,0 +1,33 @@
+package com.example.followme.db.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import com.example.followme.db.ScoreRepository;
+import com.example.followme.db.entity.ScoreEntity;
+
+public class ScoreViewModel extends AndroidViewModel {
+
+    private ScoreRepository mRepository;
+
+    private final LiveData<List<ScoreEntity>> top10;
+
+    public ScoreViewModel(Application application) {
+        super(application);
+        mRepository = new ScoreRepository(application);
+        top10 = mRepository.getTop10();
+    }
+
+    public LiveData<List<ScoreEntity>> getTop10() {
+        return top10;
+    }
+
+    public void insert(ScoreEntity score) {
+        mRepository.insert(score);
+    }
+}
+
